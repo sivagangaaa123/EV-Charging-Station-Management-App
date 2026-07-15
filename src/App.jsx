e91456e079cc516b0c4e9940665a3b421847247e
sivagangaaa123 <sivagangaaa123@gmail.com>
2026-07-15 08:35:18 +0530

    EV Charging Station View Completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,26 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import AddBooking from './components/AddBooking'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import ViewAllBookings from './components/ViewAllBookings'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-        <AddBooking/>
+     <BrowserRouter>
+     <Routes>
+      <Route path='/' element={<AddBooking/>} />
+      <Route path='/view' element={<ViewAllBookings/>} />
+     </Routes>
+     </BrowserRouter>
+
           </>
   )
 }
 
 export default App
+
+
+
